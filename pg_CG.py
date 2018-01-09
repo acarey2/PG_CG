4a45e96fbd7d0ab4bcc480b135788627abb99829
Clay18 <33457252+Clay18@users.noreply.github.com>
2018-01-09 09:31:28 -0500

    Add files via upload

---
+++ pg_CG.py
@@ -0,0 +1,21 @@
+import pyautogui as pg
+
+pg.hotkey('ctrl','winleft','d')
+pg.hotkey('winleft')
+pg.typewrite('chrome\n',0.5)
+pg.hotkey('winleft','up')
+pg.typewrite('Donald Trump\n',0.5)
+pg.hotkey('tab')
+pg.hotkey('tab')
+pg.hotkey('tab')
+pg.hotkey('tab')
+pg.hotkey('tab')
+pg.hotkey('tab')
+pg.hotkey('tab')
+pg.hotkey('tab')
+pg.hotkey('enter')
+pg.moveTo(121, 380,0.5)
+pg.click()
+
+
+
